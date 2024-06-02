2f9e30123762ccc56173975b74859233f4d479b5
Udean777 <uaart123@gmail.com>
2024-06-02 23:56:46 +0700

    feat: adding back to top btn and change some styling

--- js/script.js
+++ js/script.js
@@ -12,3 +12,21 @@ loadComponent("about", "/pages/about.html");
 loadComponent("hero", "/pages/hero.html");
 loadComponent("skill", "/pages/skills.html");
 loadComponent("projects", "/pages/projects.html");
+
+// Func back to top
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const backToTopButton = document.getElementById("back-to-top");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
